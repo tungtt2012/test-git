6155dbf413d6c367d0585287e21571e4cf7174d6
Trịnh Tiến Tùng <47631427+tungtt2012@users.noreply.github.com>
2023-03-15 13:38:25 +0700

    test

---
+++ index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import { Toastify } from 'components/atoms/Toastify';
+import { store, persistor } from 'store';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+
+root.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router>
+        <Toastify />
+        <App />
+      </Router>
+    </PersistGate>
+  </Provider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
